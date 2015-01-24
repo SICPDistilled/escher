9b218f6212cb0104ce2de058f18e4b5c2d7a6635
Gregg Williams <gregg13@innerpaths.net>
2015-01-23 21:16:09 -0800

    docs, comments, split fcn, arrow picture, cleanup
    Abstracted split-up, split-right into higher-level split function.

--- src/escher/core.clj
+++ src/escher/core.clj
@@ -6,6 +6,8 @@
 ;; Increasing x values move a point to the right.
 ;; Increasing y values move a point downward.
 
+
+;; defines size of Quil window
 (def width 600)
 (def height 600)
 
@@ -27,14 +29,11 @@
              :e2 [0 200]})
 
 
-(defn make-vect [x y]
+(defn make-vec [x y]
   [x y])
 
-
-
-(defn make-segment [p1 p2]
-  [p1 p2])
-
+(defn make-segment [vec1 vec2]
+  [vec1 vec2])
 
 (defn add-vec [[x1 y1] [x2 y2]]
   [(+ x1 x2) (+ y1 y2)]
@@ -49,10 +48,12 @@
   [(* x s) (* y s)]
   )
 
+
 (defn frame-coord-map
   "Returns function that maps [x y] relative to the frame
   defined by origin (a vector to be interpreted as the point
-  describing the origin) and vectors e1 (x-axis), e2 (y-axis)."
+  describing the origin) and vectors e1 (x-axis), e2 (y-axis).
+   See SICP, 2nd ed., page 183."
   [{:keys [origin e1 e2]}]
   (fn [[x y]]
     (add-vec origin
@@ -61,20 +62,27 @@
 
 (defn frame-painter [{:keys [origin e1 e2]}]
   "Draws parallelogram 'frame' based on origin and vectors e1 and e2.
-  Must execute within a sketch."
+  Must execute within a Quil sketch."
   (let [corner (add-vec origin (add-vec e1 e2))]
     (draw-line origin (add-vec origin e1))
     (draw-line origin (add-vec origin e2))
     (draw-line (add-vec origin e2) corner)
     (draw-line (add-vec origin e1) corner)))
 
-(defn segment-painter [segment-list]
+(defn segment-painter
+  "Returns a function that, when given a frame as its argument, draws
+  the list of line segments in that frame."
+  [segment-list]
   (fn [frame]
     (let [m (frame-coord-map frame)]
       (doseq [[start end] segment-list]
         (draw-line (m start) (m end))))))
 
-(defn transform-picture [p origin e1 e2]
+(defn transform-picture
+  "Transforms a picture by into a different picture, based on new vectors
+  for origin, e1, and e2. See SICP, 2nd ed., page 187-8. (Note that SICP
+  calls pictures 'painters' because they know how to paint themselves."
+  [p origin e1 e2]
   (fn [frame]
     (let [map (frame-coord-map frame)
           new-origin (map origin)]
@@ -106,7 +114,9 @@
       (right frame))))
 
 
-(defn below [p1 p2]
+(defn below
+  "Draws p2 below p1."
+  [p1 p2]
   (rotate (beside (rotate270 p1)
                   (rotate270 p2))))
 
@@ -130,24 +140,24 @@
       (below top
              bottom))))
 
-(defn right-split [p n]
-  (if (= n 0)
-    p
-    (let [smaller (right-split p (dec n))]
-      (beside p (below smaller smaller)))))
-
-(defn up-split [p n]
-  ;; COMPLETE (Ex 2.44)
-  )
 
 
 (defn split [f g]
-  ; COMPLETE (Ex 2.45)
-  "Should be able to do
+  "Higher-order function that allows the
+  following redefinitions:
+
     (def right-split (split beside below))
-    (def up-split (split below beside)
-  and replace the existing *-split fns"
-  )
+    (def up-split (split below beside))"
+  (fn self-fn [p n]
+    (if (= n 0)
+      p
+      (let [smaller (self-fn p (dec n))]
+        (f p (g smaller smaller))))))
+
+(def right-split (split beside below))
+(def up-split (split below beside))
+
+
 
 (defn corner-split [p n]
   (if (= n 0)
@@ -160,82 +170,118 @@
       (beside (below p top-left)
               (below bottom-right corner)))))
 
-(def combine-four (square-of-four flip-horiz
-                                  identity
-                                  rotate180
-                                  flip-vert))
+
+(def combine-four (square-of-four rotate180
+                                  flip-vert
+                                  flip-horiz
+                                  identity))
 
 (defn square-limit [p n]
   (combine-four (corner-split p n)))
-
-;; George data
-
-(def p1 (make-vect 0 0.35))
-(def p2 (make-vect 0.15 0.6))
-(def p3 (make-vect 0.3 0.4))
-(def p4 (make-vect 0.35 0.5))
-(def p5 (make-vect 0.25 1.0))
-(def p6 (make-vect 0.4 1.0))
-(def p7 (make-vect 0.5 0.7))
-(def p8 (make-vect 0.6 1.0))
-(def p9 (make-vect 0.75 1.0))
-(def p10 (make-vect 0.6 0.55))
-(def p11 (make-vect 1 0.85))
-(def p12 (make-vect 1 0.65))
-(def p13 (make-vect 0.75 0.35))
-(def p14 (make-vect 0.6 0.35))
-(def p15 (make-vect 0.65 0.15))
-(def p16 (make-vect 0.6 0))
-(def p17 (make-vect 0.4 0))
-(def p18 (make-vect 0.35 0.15))
-(def p19 (make-vect 0.4 0.35))
-(def p20 (make-vect 0.3 0.35))
-(def p21 (make-vect 0.15 0.4))
-(def p22 (make-vect 0.0 0.15))
 
 ;; Example usage of path:
 ;; (path p1 p2 p3) -->
 ;;    (([0 0.35] [0.15 0.6]) ([0.15 0.6] [0.3 0.4]))
 
-; Ex2.49, Make these shapes with segment-painter/path
-
 ;; By defining segment-lists separately, you can combine figures
 ;; by concat'ting their segment-lists--see diamond-x.
 
-(def box-segs
-  (path (make-vect 0 0) (make-vect 0.99 0)
-        (make-vect 0.99 0.99) (make-vect 0 0.99)
-        (make-vect 0 0)))
-(def box
-  (segment-painter box-segs))
 
-(def x-segs
-  (concat (path (make-vect 0 0) (make-vect 1 1))
-          (path (make-vect 1 0) (make-vect 0 1))))
-(def x
-  (segment-painter x-segs))
+;; making George drawing
 
-
-(def diamond-segs
-  (path (make-vect 0.5 0) (make-vect 1 0.5)
-        (make-vect 0.5 1) (make-vect 0 0.5)
-        (make-vect 0.5 0)))
-
-(def diamond
-  (segment-painter diamond-segs))
-
-(def diamond-x
-  (segment-painter (concat diamond-segs x-segs)))
+(def p1 (make-vec 0 0.35))
+(def p2 (make-vec 0.15 0.6))
+(def p3 (make-vec 0.3 0.4))
+(def p4 (make-vec 0.35 0.5))
+(def p5 (make-vec 0.25 1.0))
+(def p6 (make-vec 0.4 1.0))
+(def p7 (make-vec 0.5 0.7))
+(def p8 (make-vec 0.6 1.0))
+(def p9 (make-vec 0.75 1.0))
+(def p10 (make-vec 0.6 0.55))
+(def p11 (make-vec 1 0.85))
+(def p12 (make-vec 1 0.65))
+(def p13 (make-vec 0.75 0.35))
+(def p14 (make-vec 0.6 0.35))
+(def p15 (make-vec 0.65 0.15))
+(def p16 (make-vec 0.6 0))
+(def p17 (make-vec 0.4 0))
+(def p18 (make-vec 0.35 0.15))
+(def p19 (make-vec 0.4 0.35))
+(def p20 (make-vec 0.3 0.35))
+(def p21 (make-vec 0.15 0.4))
+(def p22 (make-vec 0.0 0.15))
 
 (def george-segs
-  (path  p1  p2  p3  p4  p5  p6  p7  p8  p9 p10
-        p11 p12 p13 p14 p15 p16 p17 p18 p19 p20
-        p21 p22))
+  (concat
+   (path  p1  p2  p3  p4  p5)
+   (path p6  p7  p8)
+   (path p9 p10 p11)
+   (path p12 p13 p14 p15 p16)
+   (path p17 p18 p19 p20 p21 p22)))
 
 (def george (segment-painter george-segs))
 
 
-(defn draw [picture]
+;; making arrow drawing
+
+(def length-delta 0.1)
+(def arrow-dx 0.1)
+(def arrow-dy 0.3)
+
+(def a1 (make-vec 0.5 (- 1 length-delta)))
+(def a2 (make-vec 0.5 length-delta))
+(def shaft-segs   (path a1 a2))
+
+(def a3 (make-vec (- 0.5 arrow-dx) (+ arrow-dy length-delta)))
+(def a4 (make-vec (+ 0.5 arrow-dx) (+ arrow-dy length-delta)))
+(def head-segs   (path a3 a2 a4))
+
+(def arrow (segment-painter (concat shaft-segs head-segs)))
+
+
+;; making box drawing
+
+(def box-segs
+  (path (make-vec 0 0) (make-vec 1 0)
+        (make-vec 1 1) (make-vec 0 1)
+        (make-vec 0 0)))
+(def box
+  (segment-painter box-segs))
+
+(def x-segs
+  (concat (path (make-vec 0 0) (make-vec 1 1))
+          (path (make-vec 1 0) (make-vec 0 1))))
+(def x
+  (segment-painter x-segs))
+
+
+;; making diamond drawing
+
+(def diamond-segs
+  (path (make-vec 0.5 0) (make-vec 1 0.5)
+        (make-vec 0.5 1) (make-vec 0 0.5)
+        (make-vec 0.5 0)))
+
+(def diamond
+  (segment-painter diamond-segs))
+
+
+;; making diamond-x drawing
+
+(def diamond-x
+  (segment-painter (concat diamond-segs x-segs)))
+
+
+;; making diag drawing
+
+(def diag (segment-painter [[[0 0] [1 1]]]))
+
+
+
+(defn draw
+  "Draws picture, using the entire Quil window."
+  [picture]
   (picture whole-window))
 
 (defn image-painter [img]
@@ -248,9 +294,10 @@
       ; COMPLETE
       )))
 
-(def diag (segment-painter [[[0 0] [1 1]]]))
-
-(defn draw-image []
+(defn draw-image
+  "'Container' for executing drawings within a Quil sketch.
+  Experiment by uncommenting one or more lines, or add your own."
+  []
   (let [man (image-painter (q/load-image "data/man.gif"))
         bruce (image-painter (q/load-image "data/bruce.jpg"))
         angels (image-painter (q/load-image "data/angels.jpg"))]
@@ -262,7 +309,9 @@
     ;; (draw (rotate180 george))
     ;; (draw (beside (flip-horiz george) (flip-vert george)))
     ;; (draw (beside (flip-horiz george) george))
-    (draw (below (flip-horiz george) george))
+    ;; (draw (below (flip-horiz george) george))
+    ;; (draw (right-split george 3))
+    ;; (draw (up-split george 3))
     ;; (draw (beside box x))
     ;; (draw (flip-vert george))
     ;; (draw (beside box box))
@@ -275,13 +324,14 @@
     ;; (draw ((square-of-four identity flip-vert
     ;;                        flip-horiz rotate)
     ;;        george))
+    (draw (square-limit arrow 4))
+
 
     ; Needs image-painter
     ;; (bruce frame1)
     ;; (bruce frame2)
     ;; (draw (beside george bruce))
-    ;; (draw (corner-split bruce 4))
-    ;; (draw (square-limit bruce 3))
+    ;; (draw (corner-split george 4))
     ;; (draw (beside  bruce (below  bruce
     ;;                              george)))
     ))
